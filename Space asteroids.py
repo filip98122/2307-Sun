053236c894ff5e197c98beb8b0a7a0bec7001687
filip98122 <filip98122@gmail.com>
2023-08-27 09:03:45 +0200

    making laser

--- Space asteroids.py
+++ Space asteroids.py
@@ -28,29 +28,51 @@ class Player:
         self.speed = speed
     def draw(self,window):
         pygame.draw.rect(window, pygame.Color("White"), pygame.Rect(self.x - self.width / 2, self.y - self.height / 2, 100, 100)) # Draws a rectangle
-        pass
+        
     def move(self):
-        self.dx = 0
-        self.dy = 0
+        
         if self.x > 75:
-            if keys[pygame.K_LEFT]:
-                self.x -= self.speed
+            if keys[pygame.K_a] == False:
+                if keys[pygame.K_LEFT]:
+                    self.x -= self.speed
                 
         if self.x < 975:
-            if keys[pygame.K_RIGHT]:
-                self.x += self.speed
+            if keys[pygame.K_d] == False:
+                if keys[pygame.K_RIGHT]:
+                    self.x += self.speed
                 
         if self.x > 75:
-            if keys[pygame.K_a]:
-                self.x -= self.speed
+            if keys[pygame.K_LEFT] == False:
+                if keys[pygame.K_a]:
+                    self.x -= self.speed
                 
         if self.x < 975:
-            if keys[pygame.K_d]:
-                self.x += self.speed
+            if keys[pygame.K_RIGHT] == False:
+                if keys[pygame.K_d]:
+                    self.x += self.speed
+
+class Laser:
+    def __init__(self,x,y,speed,width,height,active):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.width = width
+        self.height = height
+        self.active = active
+        self.rect = pygame.draw.rect(window, pygame.Color("White"), pygame.Rect(self.x - self.width / 2, self.y - self.height / 2, 100, 100))
+    
+    def draw(self,window):
+        pygame.draw.rect(window, pygame.Color("White"), pygame.Rect(self.x - self.width / 2, self.y - self.height / 2, 20, 100)) # Draws a rectangle
+    def move(self):
+        self.y -= self.speed
+        
+
 
 clock = pygame.time.Clock()
 p1 = Player(500,900,150,50,5.5)
-                
+laser = Laser(p1.x + 10,900,5.5,25,100,0)
+
+
 while True:
     window.fill("Blue" ) # Resets window
     
@@ -67,12 +89,28 @@ while True:
     #moves player
     p1.move()
     
+    if laser.y <= 0:
+        laser.active = 0
+        laser.y = p1.y
+        laser.x = p1.x
+    
+    if keys[pygame.K_SPACE]:
+        laser.active = 1
+    
+    if laser.active == 1:
+        laser.move()
+    
+    
     
     
     
     
     #draws player
     p1.draw(window)
+    
+    if laser.active == 1:
+        laser.draw(window)
+    
     
     pygame.display.update() # Updates window
     clock.tick(60)
